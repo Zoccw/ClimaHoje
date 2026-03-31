3114d91149cfda7d6a55b6a61d8a0232eb6021b5
Zoccw <150316534+Zoccw@users.noreply.github.com>
2026-03-31 00:40:53 -0300

    Commit project

---
+++ app.py
@@ -0,0 +1,110 @@
+from flask import Flask, render_template, Request, url_for, request
+from datetime import datetime, time, timedelta, timezone
+import main
+
+def check_dados(dados, *caminho):
+    for chave in caminho:
+        try:
+            dados = dados[chave]
+        except (KeyError, IndexError, TypeError):
+            print(f"Erro ao acessar chave '{chave}' nos dados: {dados}")
+            return None
+    return dados
+
+app = Flask(__name__, static_folder='static')
+@app.after_request
+def add_cache_headers(response):
+        response.headers['Cache-Control'] = 'public, max-age=2629743'
+        return response
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    clima_atual = None
+    local = None
+    tempo = None
+    periodo = None
+    condicao = None
+    if request.method == 'POST':
+
+        place = request.form.get('dado_place')
+        localresultado = main.getLatLon(place)
+        lat = check_dados(localresultado, 0, "lat")
+        lon = check_dados(localresultado, 0, "lon")
+        climaresultado = main.getWeather(lat, lon)
+
+        # Conversão de Data
+
+        hora = check_dados(climaresultado, "current", "dt")
+        offset = check_dados(climaresultado, "timezone_offset")
+        if hora is not None and offset is not None:
+            fuso_horario = timezone(timedelta(seconds=offset))
+            hora_data = datetime.fromtimestamp(hora, tz=fuso_horario)
+            hora_formatada = hora_data.strftime("%H:%M")
+            hora_fuso_formatada = hora_data.strftime("%Z")
+
+            if time(6,0) <= hora_data.time() <= time(18,0):
+                periodo = "dia"
+                condicao = {
+                "Thunderstorm": "⛈️",
+                "Drizzle": "🌧️",
+                "Rain": "🌧️",
+                "Snow": "🌨️",
+                "Mist": "🌫️",
+                "Smoke": "🌫️",
+                "Haze": "🌫️",
+                "Fog": "🌫️",
+                "Sand": "🌫️",
+                "Dust": "🌫️",
+                "Ash": "🌫️",
+                "Squall": "💨",
+                "Tornado": "🌪️",
+                "Clear": "☀️",
+            }
+            else:
+                periodo = "noite"
+                condicao = {
+                "Thunderstorm": "⛈️",
+                "Drizzle": "🌧️",
+                "Rain": "🌧️",
+                "Snow": "🌨️",
+                "Mist": "🌫️",
+                "Smoke": "🌫️",
+                "Haze": "🌫️",
+                "Fog": "🌫️",
+                "Sand": "🌫️",
+                "Dust": "🌫️",
+                "Ash": "🌫️",
+                "Squall": "💨",
+                "Tornado": "🌪️",
+                "Clear": "🌑",
+            }
+
+
+        else:
+            hora_formatada = ""
+            hora_fuso_formatada = ""
+
+        # Dicionários separados para cada função
+
+        clima_atual = {
+            "condicao": check_dados(climaresultado, "current", "weather", 0, "main"),
+            "descricao": check_dados(climaresultado, "current", "weather", 0, "description"),
+            "temp_atual": check_dados(climaresultado, "current", "temp"),
+            "temp_min": check_dados(climaresultado, "daily", 0, "temp", "min"),
+            "temp_max": check_dados(climaresultado, "daily", 0, "temp", "max"),
+        }
+
+        tempo = {
+            "time": hora_formatada,
+            "timezone": hora_fuso_formatada,
+        }
+
+        local = {
+            "country": check_dados(localresultado, 0,"country"),
+            "state": check_dados(localresultado,0, "state"),
+        }
+
+    return render_template("index.html", clima=clima_atual, localizacao=local, timedate=tempo, periodo=periodo, condicao=condicao)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ main.py
@@ -0,0 +1,51 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+API_KEY = os.getenv("CHAVE_API")
+geourl = "http://api.openweathermap.org/geo/1.0/direct"
+climaurl = "https://api.openweathermap.org/data/3.0/onecall"
+
+# local = input()
+def getLatLon(local):
+    parametros = {"q": local, "appid": API_KEY}
+    latlon = requests.get(geourl, parametros)
+    if latlon.status_code == 200:
+            dados = latlon.json()
+            if not dados:
+                print("lugar não encontrado")
+                return None, None
+            else:
+                print(dados)
+                return dados
+    else:
+        print(f'Erro: {latlon.status_code}')
+        return None, None
+
+# info = getLatLon(local)
+# lat = info[0]["lat"]
+# lon = info[0]["lon"]
+
+def getWeather(lat, lon):
+    parametros_clima = {
+        "lat": lat,
+        "lon": lon,
+        "lang": "pt_br",
+        "appid": API_KEY,
+        "units": "metric"
+    }
+    weather = requests.get(climaurl, parametros_clima)
+    if weather.status_code == 200:
+        data = weather.json()
+        print(data)
+        return data
+
+    else:
+        print(f'Erro: {weather.status_code}')
+        return None
+
+
+# getLatLon(local)
+#
+# getWeather(lat, lon)
